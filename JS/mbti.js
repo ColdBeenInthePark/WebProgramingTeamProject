747f1af8e01bd1d6cdf28d7c23ae24a87366c4d1
chanbin <chanbin987@naver.com>
2023-11-09 19:01:21 +0900

    add gpt

--- JS/mbti.js
+++ JS/mbti.js
@@ -3,58 +3,71 @@ document.addEventListener("DOMContentLoaded", function () {
     {
       question: "프로그래밍 언어 학습 방식",
       options: {
-        "A.구조적이고 체계적인 강의와 교과서를 활용하여 학습": "A",
-        "B. 직접 프로젝트를 만들어보며 학습하고 실험을 통해 개선": "B",
+        "A.구조적이고 체계적인 강의와 교과서를 활용하여 학습":
+          "구조적이고 체계적인 강의와 교과서를 활용하여 학습",
+        "B. 직접 프로젝트를 만들어보며 학습하고 실험을 통해 개선":
+          "직접 프로젝트를 만들어보며 학습하고 실험을 통해 개선",
       },
     },
     {
       question: "가장 큰 만족감을 느끼는 프로젝트",
       options: {
-        "A. 복잡한 데이터베이스 시스템을 구축하는 프로젝트": "A",
+        "A. 복잡한 데이터베이스 시스템을 구축하는 프로젝트":
+          "복잡한 데이터베이스 시스템을 구축하는 프로젝트",
         "B. 사용자 경험(UX)을 개선하는 웹 애플리케이션을 디자인하는 프로젝트":
-          "B",
+          "사용자 경험(UX)을 개선하는 웹 애플리케이션을 디자인하는 프로젝트",
       },
     },
     {
       question: "효과적인 작업 환경",
       options: {
-        "A. 조용하고 체계적인 개인 작업 공간": "A",
-        "B. 열려 있는 협업 공간에서 동료와 팀으로 일하는 것": "B",
+        "A. 조용하고 체계적인 개인 작업 공간":
+          "조용하고 체계적인 개인 작업 공간",
+        "B. 열려 있는 협업 공간에서 동료와 팀으로 일하는 것":
+          "열려 있는 협업 공간에서 동료와 팀으로 일하는 것",
       },
     },
     {
       question: "가장 열정적으로 일하는 분야",
       options: {
-        "A. 시스템 보안 및 네트워크 관리와 같은 기술적인 문제 해결": "A",
-        "B. 사용자 요구사항을 이해하고 사용자 중심의 소프트웨어 개발": "B",
+        "A. 시스템 보안 및 네트워크 관리와 같은 기술적인 문제 해결":
+          "시스템 보안 및 네트워크 관리와 같은 기술적인 문제 해결",
+        "B. 사용자 요구사항을 이해하고 사용자 중심의 소프트웨어 개발":
+          "사용자 요구사항을 이해하고 사용자 중심의 소프트웨어 개발",
       },
     },
     {
       question: "가장 가까운 관심사",
       options: {
-        "A. 하드웨어 구성 및 최적화": "A",
-        "B. 소프트웨어 개발과 알고리즘 설계": "B",
+        "A. 하드웨어 구성 및 최적화": "하드웨어 구성 및 최적화",
+        "B. 소프트웨어 개발과 알고리즘 설계": "소프트웨어 개발과 알고리즘 설계",
       },
     },
     {
       question: "프로그래밍 환경 선호도",
       options: {
-        "A. 저수준 프로그래밍 언어(C/C++)와 하드웨어 제어": "A",
-        "B. 고수준 프로그래밍 언어(Java, Python)와 응용 프로그래밍": "B",
+        "A. 저수준 프로그래밍 언어(C/C++)와 하드웨어 제어":
+          "저수준 프로그래밍 언어(C/C++)와 하드웨어 제어",
+        "B. 고수준 프로그래밍 언어(Java, Python)와 응용 프로그래밍":
+          "고수준 프로그래밍 언어(Java, Python)와 응용 프로그래밍",
       },
     },
     {
       question: "프로젝트 관리 방식 선호도",
       options: {
-        "A. 엄격한 일정과 계획을 중요시하는 프로젝트 관리 방식": "A",
-        "B. 유연하고 반응적인 프로젝트 관리 방식": "B",
+        "A. 엄격한 일정과 계획을 중요시하는 프로젝트 관리 방식":
+          "엄격한 일정과 계획을 중요시하는 프로젝트 관리 방식",
+        "B. 유연하고 반응적인 프로젝트 관리 방식":
+          "유연하고 반응적인 프로젝트 관리 방식",
       },
     },
     {
       question: "가장 흥미로운 활동",
       options: {
-        "A. 컴퓨터 그래픽스 및 렌더링과 같은 기술적 도전": "A",
-        "B. 소셜 미디어 알고리즘 및 인공 지능(AI)과 같은 혁신적인 분야": "B",
+        "A. 컴퓨터 그래픽스 및 렌더링과 같은 기술적 도전":
+          "컴퓨터 그래픽스 및 렌더링과 같은 기술적 도전",
+        "B. 소셜 미디어 알고리즘 및 인공 지능(AI)과 같은 혁신적인 분야":
+          "소셜 미디어 알고리즘 및 인공 지능(AI)과 같은 혁신적인 분야",
       },
     },
   ];
@@ -118,10 +131,20 @@ document.addEventListener("DOMContentLoaded", function () {
     updateView();
   };
   // 선택이 끝나고 최종 결과를 보여주는 함수
-  function showResult() {
-    // 배열의 합 계산
-    const totalScore = scores.reduce((acc, curr) => acc + parseInt(curr), 0);
-    alert(`최종 점수: ${totalScore}`);
+  async function showResult() {
+    const originalString = scores.join(", ");
+    const additionalString =
+      "성격을 가진 사람의 가장 적절한 하나의 MBTI는 뭘까? ISTP 처럼 딱 영어 4글자로만 답변";
+    const combinedString = originalString + additionalString;
+
+    const message = await getResult(combinedString);
+
+    alert("chat-GPT의 답변을 기다리고 있습니다 소요 시간 5~10초");
+    alert(message);
+  }
+
+  function showMBTI(result) {
+    alert(result);
   }
 
   nextButton.onclick = function () {
@@ -135,9 +158,9 @@ document.addEventListener("DOMContentLoaded", function () {
     }
     // 선택된 게 있다면 점수 불러오기
     score = score.value;
-
     // 점수 목록에 push
     scores.push(score);
+    console.log(scores);
     // 질문 번호 하나 뒤에걸로
     ++questionIndex;
     // 만약 마지막 질문이면 최종 결과를 보여줌
@@ -150,3 +173,41 @@ document.addEventListener("DOMContentLoaded", function () {
   };
   updateView();
 });
+
+async function getResult(request) {
+  const apiKey = null;
+  const endpoint = "https://api.openai.com/v1/chat/completions";
+
+  var message;
+
+  await fetch(endpoint, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${apiKey}`,
+    },
+    body: JSON.stringify({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "system",
+          content: "You are a helpful assistant.",
+        },
+        {
+          role: "user",
+          content: request,
+        },
+      ],
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      message = data.choices[0].message.content;
+      console.log("Response:", message);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+
+  return message;
+}
